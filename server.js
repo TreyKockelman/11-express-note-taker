e5099883def61af5ed86d4da30c2f51af86074d7
TreyKockelman <kockelmant@gmail.com>
2022-10-17 20:18:26 -0500

    finished projects

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 let notes = require('./db/db.json')
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
